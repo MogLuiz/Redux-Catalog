eee06668ed8f5414e90afe88feb02f4e5899ff22
Luiz Henrique <luizhjramos@outlook.com>
2021-12-13 19:14:18 -0300

    feat: Setting my state with API products

--- src/components/Catalog/index.tsx
+++ src/components/Catalog/index.tsx
@@ -1,13 +1,33 @@
 // Hooks
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { useSelector } from "react-redux";
+
+// API
+import api from "../../services/api";
 
 // Types
 import { IProduct } from "./types";
 
 const Catalog = () => {
+  // -------------------------------------------------
+  // States
+  // -------------------------------------------------
+
   const [catalog, setCatalog] = useState<IProduct[]>([]);
 
+  // -------------------------------------------------
+  // Hooks
+  // -------------------------------------------------
+
+  useEffect(() => {
+    api.get("products").then((response) => {
+      setCatalog(response.data);
+    });
+  }, []);
+
+  // -------------------------------------------------
+  // Return
+  // -------------------------------------------------
   return <h1>Catalog</h1>;
 };
 
