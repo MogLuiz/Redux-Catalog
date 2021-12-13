bda11fe81dd466e75491d4fa9104c7967d810189
Luiz Henrique <luizhjramos@outlook.com>
2021-12-12 22:30:34 -0300

    feat: connect my app with redux store

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,15 @@
+// Packages
+import { Provider } from "react-redux";
+
+// Store
+import store from "./store";
+
 function App() {
-  return <h1>Hello world</h1>;
+  return (
+    <Provider store={store}>
+      <h1>Hello world</h1>
+    </Provider>
+  );
 }
 
 export default App;
