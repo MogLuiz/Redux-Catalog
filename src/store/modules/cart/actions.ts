ab63656181cb1687c3e36ca10e9ccca716a42782
Luiz Henrique <luizhjramos@outlook.com>
2021-12-13 19:47:29 -0300

    feat: ADD_PRODUCT_TO_CART action

---
+++ src/store/modules/cart/actions.ts
@@ -0,0 +1,11 @@
+// Types
+import { IProduct } from './types';
+
+export const addProductToCart = (product: IProduct) => {
+    return {
+        type: "ADD_PRODUCT_TO_CART",
+        payload: {
+            product,
+        }
+    }
+}
