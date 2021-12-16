798efa7886608338a5928ce6354db37804d58f7f
Luiz Henrique <luizhjramos@outlook.com>
2021-12-16 18:47:12 -0300

    feat: IStoreState type

--- src/store/index.ts
+++ src/store/index.ts
@@ -4,6 +4,12 @@ import { createStore } from "redux"
 // CombineReducers
 import combineReducers from "./modules/rootReducer"
 
+// Types
+import { ICartState } from "./modules/cart/types"
+export interface IStoreState {
+    cart: ICartState;
+}
+
 const store = createStore(combineReducers)
 
 export default store
