17da0b9d6fb7413e9b120ac5bfbf4f1d2e2d9774
Luiz Henrique <luizhjramos@outlook.com>
2021-12-18 15:44:26 -0300

    fix: fixed switch reducer case

--- src/store/modules/cart/reducer.ts
+++ src/store/modules/cart/reducer.ts
@@ -15,7 +15,7 @@ const cart: Reducer<ICartState> = (state = INITIAL_STATE, action) => {
 
         switch (action.type) {
                  
-            case "ADD_PRODUCT_TO_CART": {
+            case "ADD_PRODUCT_TO_CART_SUCCESS": {
 
                 const { product } = action.payload;
 
